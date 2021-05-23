3a9ce851ec9f4dde845f0e345f528395b1ac8fc2
1219099256 <63403797+1219099256@users.noreply.github.com>
2021-05-23 19:28:27 +0800

    Update rcloneupload.sh

--- aria2/rcloneupload.sh
+++ aria2/rcloneupload.sh
@@ -2,17 +2,19 @@
 
 File="$3";
 LocalDIR="/root/downloads/";
-RemoteDIR="/root/od/";
+FileDIR="${File%/*}";
+tmpDIR="${File/#$LocalDIR}";
+RemoteDIR="/";
 
 if [ -n "`find ${LocalDIR} -name '*.rar'`" ];then
   unrar -p"mrcong.com" x "${File}" "${LocalDIR}pictures/";
   rm -rf "${File}";
-  File="${LocalDIR}pictures/";
+  FileDIR="${LocalDIR}pictures/";
   RemoteDIR="${RemoteDIR}pictures/";
-  else
-  curl -G https://sre24.com/api/v1/push \
-        -d token=1db8e4d7d5d42399cc72f26a7eac5eb8 \
-        --data-urlencode msg="$3"
 fi
 
-rclone move -v "${File}" od:/ --transfers=3;
+rclone move -v "${FileDIR}" od:"${RemoteDIR}" --transfers=3
+
+curl -G https://sre24.com/api/v1/push \
+      -d token=1db8e4d7d5d42399cc72f26a7eac5eb8 \
+      --data-urlencode msg="下载完成" -d topic="aria2"
