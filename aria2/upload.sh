ed678e4293c04da27d76c6441983d20bd141a287
1219099256 <g1219099256@gmail.com>
2021-06-27 19:15:45 +0800

    update

--- aria2/upload.sh
+++ aria2/upload.sh
@@ -4,7 +4,7 @@ File="$3";
 LocalDIR="/root/downloads/";
 FileDIR="${File%/*}";
 FileNAME="${File/#$LocalDIR}";
-RemoteDIR="/";
+RemoteDIR="/root/od/";
 
 if [ $FileDIR = /root/downloads ]; then
     if [ -n "`find ${LocalDIR} -name '*.rar'`" ];then
@@ -17,12 +17,12 @@ if [ $FileDIR = /root/downloads ]; then
     fi
 fi
 
-rclone moveto "${FileDIR}" od:"${RemoteDIR}";
+rclone move "${FileDIR}" "${RemoteDIR}";
 
 curl -G https://sre24.com/api/v1/push \
       -d token=1db8e4d7d5d42399cc72f26a7eac5eb8 \
       --data-urlencode msg="DE-${FileNAME}" -d topic="aria2";
-      
+
 cat >> /root/.aria2/upload.txt <<EOF
 $FileDIR
 EOF
