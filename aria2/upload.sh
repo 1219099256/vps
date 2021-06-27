6ad1974532961e6d83cb03516e3f058f8c0be1b0
1219099256 <63403797+1219099256@users.noreply.github.com>
2021-06-27 11:36:34 +0800

    Update and rename upload1.sh to upload.sh

---
+++ aria2/upload.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+File="$3";
+LocalDIR="/root/downloads/";
+FileDIR="${File%/*}";
+FileNAME="${File/#$LocalDIR}";
+RemoteDIR="/";
+
+if [ $FileDIR = /root/downloads ]; then
+    if [ -n "`find ${LocalDIR} -name '*.rar'`" ];then
+        unrar -p"mrcong.com" x "${File}" "${LocalDIR}pictures/";
+        rm -rf "${File}";
+        FileDIR="${LocalDIR}pictures/";
+        RemoteDIR="${RemoteDIR}pictures/";
+    else
+        FileDIR="$3";
+    fi
+fi
+
+rclone move "${FileDIR}" od:"${RemoteDIR}";
+
+curl -G https://sre24.com/api/v1/push \
+      -d token=1db8e4d7d5d42399cc72f26a7eac5eb8 \
+      --data-urlencode msg="${FileNAME}-DE" -d topic="aria2";
+      
+cat >> /root/.aria2/upload.txt <<EOF
+$FileDIR
+EOF
