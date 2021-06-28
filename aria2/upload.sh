b871455f513b7b88765f37b5a923cb4f7076b1ed
1219099256 <63403797+1219099256@users.noreply.github.com>
2021-06-28 11:30:24 +0800

    Create upload.sh

--- aria2/upload.sh
+++ aria2/upload.sh
@@ -21,7 +21,7 @@ rclone move "${FileDIR}" "${RemoteDIR}";
 
 curl -G https://sre24.com/api/v1/push \
       -d token=1db8e4d7d5d42399cc72f26a7eac5eb8 \
-      --data-urlencode msg="DE-${FileNAME}" -d topic="aria2";
+      --data-urlencode msg="[DE]-${FileNAME}" -d topic="aria2";
 
 cat >> /root/.aria2/file.txt <<EOF
 $FileDIR
