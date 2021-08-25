6243f9d78f54f80e3199b1dfd023cd9c47c7abd6
1219099256 <63403797+1219099256@users.noreply.github.com>
2021-08-25 14:18:04 +0800

    Update aria2.sh

--- aria2/aria2.sh
+++ aria2/aria2.sh
@@ -131,6 +131,8 @@ Download_aria2_conf(){
 	[[ ! -s "aria2.conf" ]] && echo -e "${Error} Aria2 配置文件下载失败 !" && rm -rf "${file}" && exit 1
 	wget --no-check-certificate -N "https://raw.githubusercontent.com/1219099256/vps/master/aria2/dht6.dat"
 	[[ ! -s "dht6.dat" ]] && echo -e "${Error} Aria2 DHT6文件下载失败 !" && rm -rf "${file}" && exit 1
+	wget --no-check-certificate -N "https://raw.githubusercontent.com/1219099256/vps/master/aria2/dht.dat"
+	[[ ! -s "dht6.dat" ]] && echo -e "${Error} Aria2 DHT文件下载失败 !" && rm -rf "${file}" && exit 1
 	wget --no-check-certificate -O upload.sh "https://raw.githubusercontent.com/1219099256/vps/master/aria2/upload.sh" && chmod +x upload.sh
 	[[ ! -s "upload.sh" ]] && echo -e "${Error} Aria2 上传脚本下载失败 !" && rm -rf "${file}" && exit 1
 	echo '' > aria2.session
